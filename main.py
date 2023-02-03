637ad6dbc9a5502d8ad3dc0c00875558affd1a11
Fl1mch1k <flimchik5756@gmail.com>
2023-02-03 17:53:54 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,53 @@
+print("CD Projekt RED")
+class CDPR:
+    employees = 800
+    games = 9
+    DLC = 3
+    country = "Poland"
+    date = 2002
+
+
+
+print(CDPR.employees)
+print(CDPR.games)
+print(CDPR.DLC)
+print(CDPR.country)
+print(CDPR.date)
+
+print("Quentin Tarantino")
+
+class Tarantino:
+    years = 59
+    height = 1.85
+    profession = "director"
+    films = 9
+    name = "Quentin"
+
+
+print(Tarantino.years)
+print(Tarantino.height)
+print(Tarantino.profession)
+print(Tarantino.films)
+print(Tarantino.name)
+
+
+print("Metallica")
+
+class Metallica:
+    genre = "metal"
+    albums = 10
+    songs = 47
+    participants = 4
+    date = 1981
+
+print(Metallica.genre)
+print(Metallica.albums)
+print(Metallica.songs)
+print(Metallica.participants)
+print(Metallica.date)
+
+
+
+
+
+
